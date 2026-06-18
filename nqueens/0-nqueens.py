f2b05163feff7d7977445a612ddc9ea165baf55b
anil-akr <4nil000@protonmail.com>
2026-06-18 14:31:55 +0200

    Done

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python3
-
+"""N Queens solver using backtracking."""
 import sys
-
-
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-
-try:
-    board_size = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
-
-if board_size < 4:
-    print("N must be at least 4")
-    sys.exit(1)
 
 
 def is_safe(queens, row, col):
@@ -26,7 +11,7 @@ def is_safe(queens, row, col):
     return True
 
 
-def solve(row, queens):
+def solve(row, queens, board_size):
     """Solve the n queens with backtracking and print each solution"""
     if row == board_size:
         print(queens)
@@ -35,8 +20,23 @@ def solve(row, queens):
     for col in range(board_size):
         if is_safe(queens, row, col):
             queens.append([row, col])
-            solve(row + 1, queens)
+            solve(row + 1, queens, board_size)
             queens.pop()
 
 
-solve(0, [])
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        board_size = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if board_size < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve(0, [], board_size)
