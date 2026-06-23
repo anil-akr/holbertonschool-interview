4ea7e35b51874f4870fddca1c0e15213b2698183
anil-akr <4nil000@protonmail.com>
2026-06-23 10:16:32 +0200

    UTF8 done

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,3 +1,40 @@
 #!/usr/bin/python3
+"""Module providing a UTF-8 encoding validation function"""
+
 
 def validUTF8(data):
+    """Determine if a given data set represents a valid UTF-8 encoding.
+
+    A character in UTF-8 can be 1 to 4 bytes long. Each integer in the
+    list represents 1 byte of data; only the 8 least significant bits
+    of each integer are considered.
+
+    Args:
+        data (list of int): list of integers, each representing 1 byte.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, False otherwise.
+    """
+    n_bytes = 0
+
+    for num in data:
+        byte = num & 0xFF
+
+        if n_bytes == 0:
+            if byte >> 7 == 0b0:
+                n_bytes = 0
+            elif byte >> 5 == 0b110:
+                n_bytes = 1
+            elif byte >> 4 == 0b1110:
+                n_bytes = 2
+            elif byte >> 3 == 0b11110:
+                n_bytes = 3
+            else:
+                return False
+
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            n_bytes = n_bytes - 1
+
+    return n_bytes == 0
